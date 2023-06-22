217fa39cc05c6999db25df03acb1be6485aada65
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-06-22 11:05:22 -0500

    made changes to array.js

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -91,13 +91,19 @@ console.log('My favorite foods are now', favoriteFoods);
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
-
+favoriteFoods.sort();
+favoriteFoods.reverse();
+console.log(favoriteFoods);
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+let text = favoriteFoods.join(' and ');
+console.log(text);
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+combineArray = favoriteFoods + ' ,' + animalArray;
+console.log(combineArray);
